0ceb09773789debae703e77d67b7e05bfe6ecf89
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-12-11 08:55:43 -0500

    fix entity

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { near, BigInt, log } from "@graphprotocol/graph-ts";
+import { near, log } from "@graphprotocol/graph-ts";
 import { Account } from "../generated/schema";
 
 export function handleReceipt(receipt: near.ReceiptWithOutcome): void {
@@ -24,27 +24,29 @@ function handleAction(
     return;
   }
 
-  const functionCall = action.toFunctionCall();
+  let account: Account
+  if (account == null) {
+    account = new Account(receipt.signerId);
+    const functionCall = action.toFunctionCall();
 
-  // change the methodName here to the methodName emitting the log in the contract
-  if (functionCall.methodName == "putDID") {
-      const receiptId = receipt.id.toHexString()
-      let account = new Account(receipt.signerId);
-      account.accountId = receipt.signerId;
-      account.actionLogs = outcome.logs
-  } else {
-    log.info("Not processed - FunctionCall is: {}", [functionCall.methodName]);
+    // change the methodName here to the methodName emitting the log in the contract
+    if (functionCall.methodName == "putDID") {
+        const receiptId = receipt.id.toHexString()
+        account.accountId = receipt.signerId;
+        account.actionLogs = outcome.logs
+    } else {
+      log.info("Not processed - FunctionCall is: {}", [functionCall.methodName]);
+    }
+
+    // change the methodName here to the methodName emitting the log in the contract
+    if (functionCall.methodName == "init") {
+        const receiptId = receipt.id.toHexString()
+        let account = new Account(receipt.signerId);
+        account.accountId = receipt.signerId;
+        account.actionLogs = outcome.logs     
+    } else {
+      log.info("Not processed - FunctionCall is: {}", [functionCall.methodName]);
+    }
+    account.save();
   }
-
-  // change the methodName here to the methodName emitting the log in the contract
-  if (functionCall.methodName == "init") {
-      const receiptId = receipt.id.toHexString()
-      let account = new Account(receipt.signerId);
-      account.accountId = receipt.signerId;
-      account.actionLogs = outcome.logs     
-      account.save();
-  } else {
-    log.info("Not processed - FunctionCall is: {}", [functionCall.methodName]);
-  }
-
 }
